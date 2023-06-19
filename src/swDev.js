15b865155ca0ff263614cba1760c8d945caef3e7
AbubakarMian <41990476+AbubakarMian@users.noreply.github.com>
2023-06-19 14:50:32 +0500

    servicw worker

---
+++ src/swDev.js
@@ -0,0 +1,13 @@
+function swDev(){
+
+    let swUrl = `${process.env.PUBLIC_URL}/sw.js`;
+
+    navigator.serviceWorker.register(swUrl).then( (response)=> {
+        console.log('response',response);
+        console.log('response scope',response.scope);
+    }).catch(err=>{
+        console.log('error',err);
+    });
+}
+
+export default swDev;
