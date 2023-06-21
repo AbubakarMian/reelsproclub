a2586ff9d69fc6113a5551c5894a4fd9a9ca7c2e
AbubakarMian <41990476+AbubakarMian@users.noreply.github.com>
2023-06-21 13:08:21 +0500

    build upload

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Users from './components/Users';
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename='/reelsproclub'>
         <Navbar bg="primary" data-bs-theme="light">
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
