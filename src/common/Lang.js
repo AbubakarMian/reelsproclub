5853e0cf6718270aedcf25de962284cfc8594748
AbubakarMian <arhambutt@hatinco.com>
2023-07-06 16:59:15 +0500

    working on lang

--- src/common/Lang.js
+++ src/common/Lang.js
@@ -15,4 +15,81 @@ lang['What are you looking for ?_ru'] = 'Что Вы ищете ?';
 lang['What do you need help with ?_en'] = ' What do you need help with ?';
 lang['What do you need help with ?_ru'] = 'Как я могу вам помочь ?';
 
+
+lang['Moving_en'] = 'Moving';
+lang['Moving_ru'] = 'Движущийся';
+
+lang['Handyman_en'] = 'Handyman';
+lang['Handyman_ru'] = 'Разнорабочий';
+
+lang['Event Planner_en'] = 'Event Planner';
+lang['Event Planner_ru'] = ' Планировщик мероприятий ';
+
+lang['Fitness Trainer_en'] = 'Fitness Trainer';
+lang['Fitness Trainer_ru'] = ' Тренер по фитнесу ';
+
+lang['Drivers & Cab_en'] = 'Drivers & Cab';
+lang['Drivers & Cab_ru'] = ' Водители и такси';
+
+lang['Tile Fixer_en'] = 'Tile Fixer';
+lang['Tile Fixer_ru'] = ' Фиксатор плитки';
+
+lang['Consultant_en'] = 'Consultant';
+lang['Consultant_ru'] = ' Консультант ';
+
+lang['Pest Control_en'] = 'Pest Control';
+lang['Pest Control_ru'] = ' Борьба с вредителями';
+
+lang['Lock Master_en'] = 'Lock Master';
+lang['Lock Master_ru'] = ' Мастер блокировки ';
+
+lang['people_en'] = 'people';
+lang['people_ru'] = ' люди ';
+
+lang['CCTV_en'] = 'CCTV';
+lang['CCTV_ru'] = 'Кабельное телевидение';
+
+lang['Welder_en'] = 'Welder';
+lang['Welder_ru'] = ' Сварщик ';
+
+lang['Cleaning_en'] = 'Cleaning';
+lang['Cleaning_ru'] = ' Уборка ';
+
+lang['Ac Serivce_en'] = 'Ac Serivce';
+lang['Ac Serivce_ru'] = 'Ас Сервис  ';
+
+lang['Electrician_en'] = 'Electrician';
+lang['Electrician_ru'] = ' Электрик ';
+
+lang['Plumber_en'] = 'Plumber';
+lang['Plumber_ru'] = ' Водопроводчик';
+
+lang['Carpenter_en'] = 'Carpenter';
+lang['Carpenter_ru'] = ' Плотник ';
+
+lang['Painter_en'] = 'Painter';
+lang['Painter_ru'] = ' Художник';
+
+lang['Web Developer_en'] = 'Web Developer';
+lang['Web Developer_ru'] = ' Веб-разработчик ';
+
+lang['Digital Marketing_en'] = 'Digital Marketing';
+lang['Digital Marketing_ru'] = ' Цифровой маркетинг';
+
+lang['Tailor_en'] = 'Tailor';
+lang['Tailor_ru'] = ' Портной ';
+
+lang['Pickup Delivery_en'] = 'Pickup Delivery';
+lang['Pickup Delivery_ru'] = ' Самовывоз';
+
+lang['Labour_en'] = 'Labour';
+lang['Labour_ru'] = ' Труд ';
+
+lang['Home&Office Repairs_en'] = 'Home&Office Repairs';
+lang['Home&Office Repairs_ru'] = ' Ремонт дома и офиса ';
+
+lang['_en'] = '';
+lang['_ru'] = '  ';
+
+
 export default lang;
