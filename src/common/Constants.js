a94198df811661304b0234cc389d37f0bb27d6f9
AmeerMaavia <65680588+AmeerMaavia@users.noreply.github.com>
2023-07-20 16:28:35 +0500

    build

--- src/common/Constants.js
+++ src/common/Constants.js
@@ -1,5 +1,5 @@
 // const base_url = 'http://localhost/reels_proclub_backend/public/api';
-const base_url = 'http://stagging.hatinco.com/reels_proclub_backend/public/api';
+const base_url = 'https://stagging.hatinco.com/reels_proclub_backend/public/api';
 
 export const Constant = {
   signup: `${base_url}/register`,
