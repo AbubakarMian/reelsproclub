b28d811637d86b18e9636a6a74a0552d87f10d59
AmeerMaavia <65680588+AmeerMaavia@users.noreply.github.com>
2023-08-17 17:53:07 +0500

    front fixes

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,62 +1,35 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
+import React, { useState, useEffect, useContext } from "react";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Offcanvas from "react-bootstrap/Offcanvas";
-import "./../styles/categories.css";
-import Carousel from "react-bootstrap/Carousel";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRightArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Modal from "react-bootstrap/Modal";
-import { useState,useEffect  } from "react";
-import Collapse from "react-bootstrap/Collapse";
-import InputGroup from "react-bootstrap/InputGroup";
-import { useNavigate  } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Nav_bar_area from './NavBar';
 import { Constant } from '../common/Constants';
-
-//------
-import {ContextApiContext} from '../context/ContextApi';
-
-import {useContext} from "react";
-import Common,{googleTranslate} from '../common/Common';
+import { ContextApiContext } from '../context/ContextApi';
 import Language_arr from "../common/Lang";
-//------
 
-function Categories(props) {
-  const max_length = 13;
+export default function Categories(props) {
   const navigate = useNavigate();
+  const { contextState } = useContext(ContextApiContext);
   const [categories, setCategories] = useState([]);
-  const { contextState, updateContextState } = useContext(ContextApiContext);
   const lang = contextState.language.prefix;
 
   useEffect(() => {
-    // Function to fetch categories from the API
     const fetchCategories = async () => {
       try {
         let access_token = contextState.user.access_token;
-        console.log('acces_token',access_token);
         const headers = {
           Accept: 'application/json',
           Authorization: access_token,
           'Authorization-secure': access_token,
           'client-id': 'reelspro-app-mobile',
         };
-        console.log('headers',headers);
         const response = await fetch(Constant.get_category, {
           method: 'GET',
           headers: headers,
         });
-  
+
         const data = await response.json();
-        console.log('datadata', data);
         setCategories(data.response);
       } catch (error) {
         console.error('Error fetching categories:', error);
@@ -68,16 +41,28 @@ function Categories(props) {
 
   const get_string_lable = (str_n) => {
     if (!str_n) {
-      return ""; // Return an empty string if str_n is undefined or null
+      return "";
     }
-  
+
     const str = Language_arr[str_n + lang];
-    return str && str.length < max_length ? str : str?.substring(0, max_length) + '....';
+    return str && str.length < 13 ? str : str?.substring(0, 13) + '....';
   };
-  
-  const navigateToPath = (path,params) => {
-    navigate(path,params);
+
+  const navigateToPath = (path, params) => {
+    navigate(path, params);
   };
+
+  // Custom function to group categories into rows
+  const groupCategoriesIntoRows = (categories, itemsPerRow) => {
+    const rows = [];
+    for (let i = 0; i < categories.length; i += itemsPerRow) {
+      rows.push(categories.slice(i, i + itemsPerRow));
+    }
+    return rows;
+  };
+
+  const categoriesPerRow = 3; // Change this to set items per row
+  const categorizedRows = groupCategoriesIntoRows(categories, categoriesPerRow);
 
   return (
     <section className="">
@@ -91,25 +76,26 @@ function Categories(props) {
             </div>
           </Col>
         </Row>
-        <Row>
-          {categories.map((category) => (
-            <Col key={category.id} className="colCat">
-              <a className="link_dec" onClick={() => navigateToPath(`/people`,{state:{category_id:category.id}})}>
-                <div className=" " >
-                {/* onClick={() => navigateToPath('/people')} */}
-                  <img className="img-fluid" src={category.avatar} alt={category.name} />
-
-                  <div className="img_box_txt">
-                    <p>{category.name}</p>
+        {categorizedRows.map((row, rowIndex) => (
+          <Row key={rowIndex}>
+            {row.map((category) => (
+              <Col key={category.id} className="colCat col-md-4">
+                <a
+                  className="link_dec"
+                  onClick={() => navigateToPath(`/people`, { state: { category_id: category.id } })}
+                >
+                  <div className="">
+                    <img className="img-fluid" src={category.avatar} alt={category.name} />
+                    <div className="img_box_txt">
+                      <p>{category.name}</p>
+                    </div>
                   </div>
-                </div>
-              </a>
-            </Col>
-          ))}
-        </Row>
+                </a>
+              </Col>
+            ))}
+          </Row>
+        ))}
       </Container>
     </section>
   );
 }
-
-export default Categories;
--- src/components/Reels.js
+++ src/components/Reels.js
@@ -11,20 +11,26 @@ import "./../styles/video-react.css";
 import Carousel from "react-bootstrap/Carousel";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
-import { faThumbsUp ,faEye ,faShare} from "@fortawesome/free-solid-svg-icons";
+import {
+  faThumbsUp,
+  faEye,
+  faArrowLeft,
+  faShare,
+} from "@fortawesome/free-solid-svg-icons";
 import { faCameraRetro } from "@fortawesome/free-solid-svg-icons";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Modal from "react-bootstrap/Modal";
-import { useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 import Collapse from "react-bootstrap/Collapse";
 import InputGroup from "react-bootstrap/InputGroup";
-import { useNavigate,useLocation} from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Player } from "video-react";
-import {useContext} from "react";
-import {ContextApiContext} from '../context/ContextApi';
-import { Constant } from '../common/Constants';
-import Nav_bar_area from './NavBar';
+import { useContext } from "react";
+import { ContextApiContext } from "../context/ContextApi";
+import { Constant } from "../common/Constants";
+import Nav_bar_area from "./NavBar";
+import { RWebShare } from "react-web-share";
 
 
 export default function Reels_page_export() {
@@ -35,30 +41,33 @@ export default function Reels_page_export() {
   const params = location.state;
   const category_id = location.state.category;
 
-  const navigateToPath = (path,params) => {
-    navigate(path,params);
+  const navigateToPath = (path, params) => {
+    navigate(path, params);
   };
-  
-  console.log('params category_id', category_id);
+
+  console.log("params category_id", category_id);
   useEffect(() => {
     const fetchData = async () => {
       try {
         let access_token = contextState.user.access_token;
-        console.log('acces_token',access_token);
+        console.log("acces_token", access_token);
         const headers = {
-          Accept: 'application/json',
+          Accept: "application/json",
           Authorization: access_token,
-          'Authorization-secure': access_token,
-          'client-id': 'reelspro-app-mobile',
+          "Authorization-secure": access_token,
+          "client-id": "reelspro-app-mobile",
         };
-        console.log('headers_people',headers);
-        const response = await fetch(`${Constant.get_category_people}/${category_id}`, {
-          method: 'GET',
-          headers: headers,
-        });
-  
+        console.log("headers_people", headers);
+        const response = await fetch(
+          `${Constant.get_category_people}/${category_id}`,
+          {
+            method: "GET",
+            headers: headers,
+          }
+        );
+
         const data = await response.json();
-        console.log('datadata_people', data);
+        console.log("datadata_people", data);
         setData(data.response);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -69,13 +78,24 @@ export default function Reels_page_export() {
   }, [params]);
   return (
     <section className="">
-      {/* <Nav_bar_area /> */}
+      <Container fluid>
+        <Row>
+          <Col>
+            <Button
+              className="profile-backbtnsignup"
+              onClick={() => navigate(-1)}
+            >
+              <FontAwesomeIcon icon={faArrowLeft} />{" "}
+            </Button>
+          </Col>
+        </Row>
+      </Container>
       <Container fluid className="reelArea">
         {/* Uncomment the following lines to display multiple reels */}
         {data.map((reel) => (
           <Row key={reel.id} className="reel_box">
             <Col className="img_adj">
-              <div className="img_area" onClick={() => navigate('/reelvideo')}>
+              <div className="img_area" onClick={() => navigate("/reelvideo")}>
                 <img src={reel.user.imageUrl} alt="Reel Thumbnail" />
               </div>
             </Col>
@@ -85,12 +105,12 @@ export default function Reels_page_export() {
                   <b>{reel.user.name}</b>
                 </p>
                 <p>
-                <FontAwesomeIcon icon={faStar} style={{ color: "#fb9d23" }} />
-                <FontAwesomeIcon icon={faStar} style={{ color: "#fb9d23" }} />
-                <FontAwesomeIcon icon={faStar} style={{ color: "#fb9d23" }} />
-                <FontAwesomeIcon icon={faStar} style={{ color: "#fb9d23" }} />
-                <FontAwesomeIcon icon={faStar} style={{ color: "#bbaeae" }} />
-              </p>
+                  <FontAwesomeIcon icon={faStar} style={{ color: "#fb9d23" }} />
+                  <FontAwesomeIcon icon={faStar} style={{ color: "#fb9d23" }} />
+                  <FontAwesomeIcon icon={faStar} style={{ color: "#fb9d23" }} />
+                  <FontAwesomeIcon icon={faStar} style={{ color: "#fb9d23" }} />
+                  <FontAwesomeIcon icon={faStar} style={{ color: "#bbaeae" }} />
+                </p>
                 <p>
                   <FontAwesomeIcon className="like" icon={faThumbsUp} />
                   {/* ({reel.likes} likes) */}
@@ -98,13 +118,33 @@ export default function Reels_page_export() {
                 </p>
               </div>
               <div className="btn_areaa">
-                <Button className="reel_btn" onClick={() => navigate('/viewreels')}>
-                  <FontAwesomeIcon icon={faEye} /><span className="btn_span"> View</span>
+                <Button
+                  className="reel_btn"
+                  onClick={() => navigate("/viewreels")}
+                >
+                  <FontAwesomeIcon icon={faEye} />
+                  <span className="btn_span"> View</span>
                 </Button>
-                <Button className="reel_btn">
-                  <FontAwesomeIcon icon={faShare} /><span className="btn_span_share"> Share</span>
-                </Button>
-                <Button className="reel_btn" onClick={() => navigateToPath(`/hire`,{state:{user:reel.user.id}})}>
+                <RWebShare
+          data={{
+            text: "Web Share",
+            url: "https://media.w3.org/2010/05/sintel/trailer_hd.mp4",
+            title: "GfG",
+          }}
+          onClick={() => console.log("shared successfully!")}
+        >
+          <Button className="reel_btn">
+            <FontAwesomeIcon icon={faShare} />
+            <span className="btn_span_share"> Share</span>
+          </Button>
+        </RWebShare>
+                
+                <Button
+                  className="reel_btn"
+                  onClick={() =>
+                    navigateToPath(`/hire`, { state: { user: reel.user.id } })
+                  }
+                >
                   Hire
                 </Button>
               </div>
@@ -155,4 +195,4 @@ const Login_form = () => {
     </>
   );
 };
-//7e1c4d  
+//7e1c4d
